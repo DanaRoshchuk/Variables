327da81170b2bad84252c77677bba4c5eef16a43
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-11-13 16:48:17 +0200

    Update main.tf

--- variable_validation/passed/bool_string/main.tf
+++ variable_validation/passed/bool_string/main.tf
@@ -21,7 +21,7 @@ variable "secondary_location" {
   description = "Partner location to use for deployment"
 
   validation {
-    condition = var.secondary_location != var.primary_location
+    condition = var.secondary_location != "eastus"
     error_message = "Secondary location must be different from the primary location"
   }
 }
