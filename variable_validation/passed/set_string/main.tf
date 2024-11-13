284108cb7e934bdce7ae5685b1a5e537d2f5a5a5
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-11-13 13:19:32 +0200

    Update main.tf

--- variable_validation/passed/set_string/main.tf
+++ variable_validation/passed/set_string/main.tf
@@ -4,7 +4,7 @@ variable "magic_animals_set" {
   default = ["unicorn$"]
 
   validation {
-    condition     = regex("[a-z]+", one(toset(["hello"]))) == "unicorn"
+    condition     = regex("[a-z]+", one(toset(var.magic_animals_set))) == "unicorn"
     # Not allowed due to the use of a variable:
     # error_message = "Invalid value [${var.prefix}]. The prefix value can only contain letters and numbers."
     error_message = "The value can only contain letters and numbers."
