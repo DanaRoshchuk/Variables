50148d9c0b093ce07696012e3a67eca413edb5da
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2024-11-26 10:56:19 +0200

    Update main.tf

--- variable_validation/passed/diff_type_validation/main.tf
+++ variable_validation/passed/diff_type_validation/main.tf
@@ -103,3 +103,12 @@ validation {
     error_message = "<helpful error message>"
   }
 }
+variable "string_like_valid_ipv4_cidr" {
+  type    = string
+  default = "10.12.127.0/20"
+
+  validation {
+    condition     = cidrhost(var.string_like_valid_ipv4_cidr, 16) == "10.12.112.16"
+    error_message = "Must be valid IPv4 CIDR."
+  }
+}
