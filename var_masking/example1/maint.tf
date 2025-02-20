806dee0a12dd0556b4c85fc64bba55b27eb86641
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-02-20 16:58:16 +0200

    Create maint.tf

---
+++ var_masking/example1/maint.tf
@@ -0,0 +1,18 @@
+resource "random_pet" "resource" {
+  count = 1
+  keepers = {
+    string = var.var-var-var
+  }
+  length = var.len
+}
+
+variable "var-var-var" {
+  sensitive = true
+  default = "string"
+}
+
+
+variable "len" {
+  sensitive = true
+  default = 1234
+}
